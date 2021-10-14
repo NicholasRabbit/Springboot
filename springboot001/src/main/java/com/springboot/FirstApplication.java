54a69b92f0cdcd409a955f6f7e3fb29322244b22
plms <askdell@sina.com>
2021-10-14 09:52:54 +0800

    jar microservice test

--- springboot001/src/main/java/com/springboot/FirstApplication.java
+++ springboot001/src/main/java/com/springboot/FirstApplication.java
@@ -11,6 +11,15 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 5，但是MyBatis的映射文件xxxMapper.xml还需要配置，xxxMapper.java还是在dao层
 * */
 
+/*6，springboot可把项目打成jar包，从而简化部署，见pom.xml里的配置
+使用步骤：
+(1),pom.xml里配置相关依赖“<artifactId>spring-boot-maven-plugin</artifactId>”
+(2),运行maven构建项目的步骤，打包项目，打成的jar包在target目录下
+(3),直接java -jar spring-boot-01-helloworld-1.0-SNAPSHOT.jar即可
+实际相当于启动了一个独立的小型的web项目模块，在浏览器输入localhost:8080/first就能访问(这属于一个微服务)
+* */
+
+
 
 @SpringBootApplication     //这个注解表示这个一个springboot主程序类，表名这是一个springboot应用
 public class FirstApplication {
