dc994f536f9d0f3b4ee867281d81730816e1d72f
plms <askdell@sina.com>
2021-11-12 16:43:12 +0800

    SpringBoot自动配置原理研究-3

--- springboot007/src/main/java/com/spring/boot/autoConfig/SpringBootApplication007.java
+++ springboot007/src/main/java/com/spring/boot/autoConfig/SpringBootApplication007.java
@@ -10,7 +10,12 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  *       自定义编码格式，底层与SpringMVC的CharacterEncodingFilter设置一样
  * 3，参照注解@SpringBootApplicatin-->@EnableAutoConfiguration-->@AutoConfigurationImportSelector
  *    其它参照笔记及源码
- * 4，
+ * 4，这些自动配置类跟springboot003里的CustomerConfig.java配置类一样，替代了spring.xml配置文件，
+ *    SpringBoot已经写好了很多自动配置类，会根据每个jar包下/META-INF/xxx.properties的的设置，给相关项赋值
+ *    例如：org.springframework.boot:autoconfigure包下的一个编码设置类，跟以前SpringMVC的CharacterEncodingFilter功能一样，过滤编码
+ *    prefix引入配置文件中“spring.http.encoding = xxx”的值
+ *    @ConditionalOnProperty(prefix = "spring.http.encoding", value = "enabled", matchIfMissing = true)
+ *    public class HttpEncodingAutoConfiguration {}
  * */
 
 @SpringBootApplication
