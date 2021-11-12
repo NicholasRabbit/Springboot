da11573fc77394933ead964161a62ceed2262c61
plms <askdell@sina.com>
2021-11-12 17:22:53 +0800

    springboot日志框架-1

--- springboot008/src/main/java/com/spring/boot/log/SpringBootApplication008.java
+++ springboot008/src/main/java/com/spring/boot/log/SpringBootApplication008.java
@@ -3,6 +3,12 @@ package com.spring.boot.log;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+/**
+ * SpringBoot的默认日志框架是SLF4J(接口层)和logback(实现层)
+ * Spring底层所用的框架是Jakarta Commons Logging(JCL)
+ * 因此，SpringBoot为不同的日志框架配置了转接器，这个转接器(实际是接口适配器)上接JCL下接SLF4J，这就可使用SpringBoot本身的日志框架了
+ * */
+
 @SpringBootApplication
 public class SpringBootApplication008 {
 
