7c4d62601e666096d138bbe5cc87bde7d5be0eff
plms <askdell@sina.com>
2023-07-25 11:18:50 +0800

    move some files to another repository

--- SpringBoot笔记/SpringBoot创建线程代码范例/RecycleOrderController.java
+++
@@ -1,9 +0,0 @@
-
-
-//同步单个订单
-	@PostMapping(value="/syncOrderToWms")
-	@ResponseBody
-	public String syncOrderToWms(RecycleOrder recycleOrder){
-		wmsAsyncService.transferOrderToWms(recycleOrder);
-		return renderResult(Global.TRUE, text("同步成功！"));
-	}
